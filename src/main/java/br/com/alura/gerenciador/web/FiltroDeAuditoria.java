b7a69d21d23e21333c1739ab79b1f88630ec9799
Aldecir Neves de Lima <aldecirlima@gmail.com>
2022-06-12 12:30:03 -0300

    Aula 4 atividade 1 completada

---
+++ src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -0,0 +1,25 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter(urlPatterns = "/*")
+public class FiltroDeAuditoria implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		String uri = req.getRequestURI();
+		System.out.println("Usuário acessando a URI " + uri);
+		chain.doFilter(request, response);
+	}
+
+}
